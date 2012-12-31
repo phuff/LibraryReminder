9b2e4fa6da5b19e0d07948364eef63a9d389b66c
Paul Huff <paul.huff@gmail.com>
2012-12-31 14:22:38 -0700

    First round of changes to handle deletion of calendar events.  Deletion not quite working yet.

--- LibraryReminder.py
+++ LibraryReminder.py
@@ -3,6 +3,7 @@
 from mechanize import Browser
 from BeautifulSoup import BeautifulSoup
 from elementtree import ElementTree
+import json
 
 try:
   from xml.etree import ElementTree
@@ -26,7 +27,7 @@ class HorizonInterface:
         self.browser["sec2"] = self.password
         self.browser.submit()
 
-        
+
     def getCheckedOutBooks(self):
         response = self.browser.follow_link(text_regex=r"^Items Out$")
         bs = BeautifulSoup(response.read())
@@ -52,24 +53,31 @@ class GoogleCalendarInterface:
         self.client = gdata.calendar.client.CalendarClient(source='org.phuff-HorizonGCalInterface-v1')
         self.client.ClientLogin(username, password, self.client.source)
         self.booksDueTitle = 'Library Books Due'
-    
+
+
+    def getFeedForDate(self, dueDate):
+      (month, day, year) = dueDate.split('/')
+      googleStartDate = '%s-%s-%02d' % (year,month,int(day))
+      googleEndDate = '%s-%s-%02d' % (year,month,int(day) + 1)
+      query = gdata.calendar.client.CalendarEventQuery()
+      query.start_min = googleStartDate
+      query.start_max = googleEndDate
+      feed = self.client.GetCalendarEventFeed(q=query)
+      return feed
+
     def ensureAppropriateEventsExistForBooks(self, books):
         for dueDate in books:
+          feed = self.getFeedForDate(dueDate)
+          found = False
+
+          for event in feed.entry:
+            if event.title.text == self.booksDueTitle:
+              self.ensureEventIsAccurate(event, books[dueDate])
+              found = True
+          if not found:
             (month, day, year) = dueDate.split('/')
             googleStartDate = '%s-%s-%02d' % (year,month,int(day))
-            googleEndDate = '%s-%s-%02d' % (year,month,int(day) + 1)
-            query = gdata.calendar.client.CalendarEventQuery()
-            query.start_min = googleStartDate
-            query.start_max = googleEndDate
-            feed = self.client.GetCalendarEventFeed(q=query)
-            found = False
-
-            for event in feed.entry:
-                if event.title.text == self.booksDueTitle:
-                    self.ensureEventIsAccurate(event, books[dueDate])
-                    found = True
-            if not found:
-                self.createEvent(googleStartDate, books[dueDate])
+            self.createEvent(googleStartDate, books[dueDate])
 
     def createEvent(self, date, booksForDate):
         event = gdata.calendar.data.CalendarEventEntry()
@@ -77,17 +85,20 @@ class GoogleCalendarInterface:
         content = ""
         for book in booksForDate:
             content += "%s %s %s\n" % (book[0], book[1], book[3], )
-        
+
         event.content = atom.data.Content(text=content)
         when = gdata.calendar.data.When(start=date, end=date)
         when.reminder.append(gdata.data.Reminder(days='1'))
         event.when.append(when)
         new_event = self.client.InsertEvent(event)
 
+    def getCalendarTextForBook(self, book):
+      return "%s %s %s" % (book[0], book[1], book[3], )
+
     def ensureEventIsAccurate(self, event, booksForDate):
         dirty = False
         for book in booksForDate:
-            line = "%s %s %s" % (book[0], book[1], book[3], )
+            line = self.getCalendarTextForBook(book)
             if event.content.text.find(line) < 0:
                 dirty = True
                 content = event.content.text
@@ -96,15 +107,64 @@ class GoogleCalendarInterface:
         if dirty:
             self.client.Update(event)
 
+    def bookIsDue(self, oldBook, dueDate, currentBooks):
+      if not currentBooks.has_key(dueDate):
+        return False
+
+      for book in currentBooks[dueDate]:
+        if oldBook == book:
+          return True
+
+      return False
+
+
+    def removeBookFromFeed(self, book, dueDate):
+      feed = self.getFeedForDate(dueDate)
+      for event in feed.entry:
+        if event.title.text == self.booksDueTitle:
+          line = self.getCalendarTextForBook(book)
+          newContent = event.content.text.replace(line + "\n", "")
+          if newContent.strip() == '':
+            self.client.Delete(event)
+          elif newContent != event.content.text:
+            event.content = atom.data.Content(text=newContent)
+            self.client.Update(event)
+
+    def removeOldBooks(self, oldBooks, currentBooks):
+      for dueDate in oldBooks:
+        for book in oldBooks[dueDate]:
+          if not self.bookIsDue(book, dueDate, currentBooks):
+            self.removeBookFromFeed(book, dueDate)
 
 if __name__ == '__main__':
-    library_accounts = open("library_accounts.txt")
-    for line in library_accounts:
-        (library_site, library_account, library_pin, gcal_account, gcal_password) = line.split(',')
-        horizon = HorizonInterface(library_account, library_pin, library_site)
-        books = horizon.getCheckedOutBooks()
-        gcalInterface = GoogleCalendarInterface(gcal_account, gcal_password)
-        gcalInterface.ensureAppropriateEventsExistForBooks(books)
 
-    
+  def merge_dictionaries(dict1, dict2):
+    '''Merges book duedate dict2 into dict1 and returns dict1'''
+    for dueDate in dict2:
+      if not dueDate in dict1:
+        dict1[dueDate] = []
 
+      for book in dict2[dueDate]:
+        dict1[dueDate].append(book)
+
+  library_accounts = open("library_accounts.txt")
+
+  allBooks = {}
+
+  for line in library_accounts:
+    (library_site, library_account, library_pin, gcal_account, gcal_password) = line.split(',')
+    horizon = HorizonInterface(library_account, library_pin, library_site)
+    books = horizon.getCheckedOutBooks()
+    gcalInterface = GoogleCalendarInterface(gcal_account, gcal_password)
+    gcalInterface.ensureAppropriateEventsExistForBooks(books)
+    merge_dictionaries(allBooks, books)
+
+  try:
+    with open("library_books.json") as f:
+      oldBooks = json.load(f)
+  except IOError as e:
+    oldBooks = {}
+
+  gcalInterface.removeOldBooks(oldBooks, allBooks)
+  with open("library_books.json", "w") as f:
+    f.write(json.dumps(allBooks))
